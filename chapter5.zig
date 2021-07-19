2a4768a692b5a16888273ede3c4d16aa3e97ab78
vanadium23 <chernoffivan@gmail.com>
2021-07-19 22:03:25 +0300

    [Feat] one test from async chapter

---
+++ chapter5.zig
@@ -0,0 +1,10 @@
+const expect = @import("std").testing.expect;
+
+fn add(a: i32, b: i32) i64 {
+    return a + b;
+}
+
+test "@Frame" {
+    var frame: @Frame(add) = async add(1, 2);
+    try expect(await frame == 3);
+}
