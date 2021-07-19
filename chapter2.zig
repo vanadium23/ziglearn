29ef264843f771603b28cf66c53025478443ff31
vanadium23 <chernoffivan@gmail.com>
2021-07-19 21:58:07 +0300

    [Feat] add chapter 2

---
+++ chapter2.zig
@@ -0,0 +1,105 @@
+const expect = @import("std").testing.expect;
+const std = @import("std");
+const eql = std.mem.eql;
+const test_allocator = std.heap.page_allocator;
+
+test "createFile, write, seekTo, read" {
+    const file = try std.fs.cwd().createFile(
+        "junk_file.txt",
+        .{ .read = true },
+    );
+    defer file.close();
+
+    const bytes_written = try file.writeAll("Hello File!");
+
+    var buffer: [100]u8 = undefined;
+    try file.seekTo(0);
+    const bytes_read = try file.readAll(&buffer);
+
+    try expect(eql(u8, buffer[0..bytes_read], "Hello File!"));
+}
+
+
+test "hello world" {
+    const out_file = std.io.getStdOut();
+    try out_file.writer().print(
+        "Hello, {s}!\n",
+        .{"World"},
+    );
+}
+
+test "array printing" {
+    const string = try std.fmt.allocPrint(
+        test_allocator,
+        "{any} + {any} = {any}",
+        .{
+            @as([]const u8, &[_]u8{ 1, 4 }),
+            @as([]const u8, &[_]u8{ 2, 5 }),
+            @as([]const u8, &[_]u8{ 3, 9 }),
+        },
+    );
+    defer test_allocator.free(string);
+
+    try expect(eql(
+        u8,
+        string,
+        "{ 1, 4 } + { 2, 5 } = { 3, 9 }",
+    ));
+}
+
+const Place = struct { lat: f32, long: f32 };
+
+test "json parse" {
+    var stream = std.json.TokenStream.init(
+        \\{ "lat": 40.684540, "long": -74.401422 }
+    );
+    const x = try std.json.parse(Place, &stream, .{});
+
+    try expect(x.lat == 40.684540);
+    try expect(x.long == -74.401422);
+}
+
+test "fetchPut" {
+    var map = std.AutoHashMap(u8, f32).init(
+        test_allocator,
+    );
+    defer map.deinit();
+
+    try map.put(255, 10);
+    const old = try map.fetchPut(255, 100);
+
+    try expect(old.?.value == 10);
+    try expect(map.get(255).? == 100);
+}
+
+test "stack" {
+    const string = "(()())";
+    var stack = std.ArrayList(usize).init(
+        test_allocator,
+    );
+    defer stack.deinit();
+
+    const Pair = struct { open: usize, close: usize };
+    var pairs = std.ArrayList(Pair).init(
+        test_allocator,
+    );
+    defer pairs.deinit();
+
+    for (string) |char, i| {
+        if (char == '(') try stack.append(i);
+        if (char == ')')
+            try pairs.append(.{
+                .open = stack.pop(),
+                .close = i,
+            });
+    }
+
+    for (pairs.items) |pair, i| {
+        try expect(std.meta.eql(pair, switch (i) {
+            0 => Pair{ .open = 1, .close = 2 },
+            1 => Pair{ .open = 3, .close = 4 },
+            2 => Pair{ .open = 0, .close = 5 },
+            else => unreachable,
+        }));
+    }
+}
